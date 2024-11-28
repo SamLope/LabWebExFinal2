bf47f30ea907e8dc958932abca29894cf0a84494
Samlope <samir.lopes@hotmail.com>
2024-11-28 15:26:46 -0300

    Movendo os arquivos para Raiz do projeto

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,10 @@
 const aluno = require('../api/v1/alunos/aluno-routes');
+const produto = require('../api/v1/produtos/produto-routes');
 
 const routes = [
-    ...aluno
+    ...aluno,
+    ...produto
+    
 ]
 
 module.exports = routes;
